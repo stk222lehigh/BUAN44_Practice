bf9bbd6391d5900ff62c54f2de3672eaebb3eb0b
stk222lehigh <111547238+stk222lehigh@users.noreply.github.com>
2022-08-18 21:34:54 -0400

    Add files via upload

---
+++ GitPractice.R
@@ -0,0 +1,16 @@
+#This is a sample R Script for exercise in Git
+
+#Load documentation from R's pre-loaded dataset
+#Documentation appears in the help pane on RStudio
+?mtcars
+
+#Inspect the dataset in a view pane
+View(mtcars)
+
+#Calculate the average mpg of all cars in this dataset
+mean(mtcars$mpg)
+
+#Create a histogram for mpg variable in mtcars
+hist(mtcars$mpg)
+
+#Add some more code below as instructed, then commit to your repository
